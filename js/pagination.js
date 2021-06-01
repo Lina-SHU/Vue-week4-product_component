f490fbbe6ebd42430b649411ed6121d08987f257
Lina Chen <sakura0477@gmail.com>
2021-06-01 15:28:12 +0800

    update component and product

--- js/pagination.js
+++ js/pagination.js
@@ -1,7 +1,7 @@
 export default {
   props: ['pages'],
   template: `
-        <div class="d-flex justify-content-center">
+        <div class="d-flex justify-content-center mt-3">
           <nav aria-label="Page navigation example">
             <ul class="pagination">
               <li class="page-item" :class="{disabled: !pages.has_pre}"><a class="page-link" href="#" @click="$emit('getProduct', pages.current_page -1)">Previous</a></li>
